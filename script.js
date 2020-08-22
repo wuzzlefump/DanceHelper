7cbdedcce8a2ee71253d75578db570125c170033
wuzzlefump <paulelliottwork@gmail.com>
2020-08-22 16:49:08 -0600

    display Rhythm and Smooth

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ this.array = array;
 this.extra = extra;
 this.picture = picture;
 }
+function DANCE(name,description,array,picture,extra){
+
+    this.name = name;
+    this.description = description;
+    this.array= array;
+    this.picture=picture;
+    this.extra=extra;
+}
+
 
 
 //home
@@ -21,11 +30,31 @@ const American = new Category("American","American style dances refer to dances 
 //Rhythm
 const Rhythm = new Category("Rhythm", "Rhythm Dances",["ChaCha","Rumba","EastCoastSwing","Bolero","Mambo","American"],"Rhythm Dances", `<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`)
 
+const ChaCha = new DANCE("ChaCha","description",["Rhythm"],`<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"7/10"+" its versatility is: "+ "above average"+" and its song tempo plus an example is: "+" 4/4 time 30-32 Mpm Major: Love Me Ole" )
+
+const Rumba = new DANCE("Rumba","description",["Rhythm"],`<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"5/10"+" its versatility is: "+"Very Versatile" +" and its song tempo plus an example is: "+" 4/4 time 30-32 Mpm  Ally Brooke: Low Key" )
+
+const EastCoastSwing = new DANCE("EastCoastSwing", "description",["Rhythm"],`<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"6/10"+" its versatility is: "+ "Very Versatile"+" and its song tempo plus an example is: "+"4/4 time 32-36 Mpm Flo Rida: Dirty Mind" )
+
+const Bolero = new DANCE("Bolero", "description",["Rhythm"],`<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`,"Low","Pauls Biased belief of this dance is that its difficulty is: "+"8/10"+" its versatility is: "+" Low"+" and its song tempo plus an example is: "+" 4/4 time 24-26 Mpm SYML: Mr Sandman" )
+
+const Mambo = new DANCE("Mambo","description",["Rhythm"],`<img src="https://img.icons8.com/emoji/100/000000/dancing-girl.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"7/10"+" its versatility is: "+"average" +" and its song tempo plus an example is: "+" 4/4 47-51 mpm time Cali Flow: Ras Tas Tas")
 //Rhythm
+
+// other
 const Other = new Category("Other", "other",["StreetLatin","Club","Country","Misc", "Home"], "other", `<i class="fas fa-road fa-10x"></i>`)
+//other
 
 //Smooth
 const Smooth = new Category("Smooth","Smooth Dancing",["Waltz","Tango","Foxtrot","VienneseWaltz","American"],"Smooth Dances", `<img src="https://img.icons8.com/ios-filled/100/000000/ballroom-dance.png"/>`)
+
+const Waltz = new DANCE("Waltz","description",["Smooth"],`<img src="https://img.icons8.com/ios-filled/100/000000/ballroom-dance.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"5/10"+" its versatility is: "+"low" +" and its song tempo plus an example is: "+" 3/4 time 30-32 Mpm Chris Spheeris: Enchantment ")
+
+const Tango = new DANCE("Tango","description",["Smooth"],`<img src="https://img.icons8.com/ios-filled/100/000000/ballroom-dance.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"6/10"+" its versatility is: "+"above average" +" and its song tempo plus an example is: "+"4/4 time 30-32 Mpm Gotan Project: Santa Maria" )
+
+const Foxtrot = new DANCE("Foxtrot","description",["Smooth"],`<img src="https://img.icons8.com/ios-filled/100/000000/ballroom-dance.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+"7/10"+"  its versatility is: "+"average"+" and its song tempo plus an example is: "+"30-34 mpm  Three Dog Night: One" )
+
+const VienneseWaltz = new DANCE("VienneseWaltz","description",["Smooth"],`<img src="https://img.icons8.com/ios-filled/100/000000/ballroom-dance.png"/>`,"Pauls Biased belief of this dance is that its difficulty is: "+" 8.5/10"+" its versatility is: "+"average"+" and its song tempo plus an example is: "+ "3/4 time 54-46 Mpm 5 seconds of Summer:Ghost of you")
 
 //Smooth
 
@@ -76,7 +105,7 @@ function clearContent(){
 }
 
 $(document).on("click", ".button", function(){
-    const objectArray = [Home,American,International,Other,Rhythm,Smooth,Alternatives,Standard,Latin]
+    const objectArray = [Home,American,International,Other,Rhythm,ChaCha,Rumba,EastCoastSwing,Bolero,Mambo,Smooth,Waltz,Tango,Foxtrot,VienneseWaltz,Alternatives,Standard,Latin]
     const target = $(this).attr("data-name");
     console.log(target);
     objectArray.forEach(function(item){
